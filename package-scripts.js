f04a3081cf342d974469f68a29625d4f8be1013a
devdigital <devdigital.cloud@gmail.com>
2017-09-02 10:07:12 +0100

    Added get next version

--- package-scripts.js
+++
@@ -1,8 +0,0 @@
-const getStandardVersion = require('./scripts/standard-version-utils')
-  .getStandardVersion
-
-module.exports = {
-  scripts: {
-    'get-standard-version': getStandardVersion(),
-  },
-}
